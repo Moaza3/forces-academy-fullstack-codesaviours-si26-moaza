c6a99d57ea367ff0709d756dcc078f0cc64ccdb8
Moaza3 <moazayaqoob03@gmail.com>
2026-09-07 16:02:53 +0500

    Add timetable functionality and sample data

--- timetable.php
+++ timetable.php
@@ -77,7 +77,7 @@ if (!empty($student_class)) {
                 <a href="courses.php" class="nav-link text-white">My Courses</a>
             </li>
             <li class="nav-item mb-2">
-                <a href="timetable.php" class="nav-link text-white">📅 Timetable</a>
+                <a href="timetable.php" class="nav-link text-white">Timetable</a>
             </li>
             <li class="nav-item mb-2">
                 <a href="assignment.php" class="nav-link text-white">Assignments</a>
